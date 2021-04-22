d4ba787c46a6dd8e2118d8fb9fcc532e2f015cbf
Primosz <valadin96@gmail.com>
2021-04-22 21:18:21 +0200

    Added triangular and trapezoidal

--- doggos/utils/membership_functions/membership_functions.py
+++ doggos/utils/membership_functions/membership_functions.py
@@ -1,4 +1,4 @@
-from numpy import exp
+from numpy import exp, minimum, maximum
 from numpy.core.function_base import linspace
 
 
@@ -18,7 +18,7 @@ def gaussian(domain, mean, sigma, max_value=1):
       sigma:
         Standard deviation of gaussian function
       max_value:
-      Maximum value of membership function, height.
+        Maximum value of membership function, height.
 
     Returns:
       ndarray
@@ -64,3 +64,76 @@ def sigmoid(domain, offset, magnitude):
       >>> sigmoid_set = sigmoid(x1, 0.5, -15)
     """
     return 1. / (1. + exp(- magnitude * (domain - offset)))
+
+
+def triangular(domain, l_end, center, r_end, max_value=1):
+    """Triangular membership function
+
+    Defines membership function of triangular shape
+    by a vector of values. To be used to determine membership value of crisp number
+    to fuzzy set defined by this function.
+
+    Args:
+      domain:
+        Array-like input which indicate the points from universe for which
+        the membership function will be evaluated.
+      l_end:
+        Left end, vertex of triangle, where value of function is equal to 0.
+      center:
+        Top vertex of triangle, where value of function is equal to 1.
+      r_end:
+        Right end, vertex of triangle, where value of function is equal to 0.
+      max_value:
+        Maximum value of membership function, height.
+
+    Returns:
+      ndarray
+      A vector of membership values corresponding to input. Number of values
+      defined implicitly by domain argument. Each value contains calculated membership degree
+      to fuzzy set defined by the membership function.
+
+
+    Example usage:
+      >>> x = linspace(0, 1, 100)
+      >>> triangle_set = triangular(x1, 0.2, 0.3, 0.7)
+    """
+    return minimum(1, maximum(0, ((max_value * (domain - l_end) / (center - l_end)) * (domain <= center) +
+                                  ((max_value * ((r_end - domain) / (r_end - center))) * (domain > center)))))
+
+
+def trapezoidal(domain, l_end, l_center, r_center, r_end, max_value=1):
+    """Triangular membership function
+
+    Defines membership function of trapezoidal shape
+    by a vector of values. To be used to determine membership value of crisp number
+    to fuzzy set defined by this function.
+
+    Args:
+      domain:
+        Array-like input which indicate the points from universe for which
+        the membership function will be evaluated.
+      l_end:
+        Left end, vertex of trapezoid, where value of function is equal to 0.
+      l_center:
+        Top left vertex of trapezoid, where value of function is equal to 1.
+      r_center:
+        Top right vertex of triangle, where value of function is equal to 1.
+      r_end:
+        Right end, vertex of trapezoid, where value of function is equal to 0.
+      max_value:
+        Maximum value of membership function, height.
+
+    Returns:
+      ndarray
+      A vector of membership values corresponding to input. Number of values
+      defined implicitly by domain argument. Each value contains calculated membership degree
+      to fuzzy set defined by the membership function.
+
+
+    Example usage:
+      >>> x = linspace(0, 1, 100)
+      >>> trapezoid_set = trapezoidal(x1, 0.2, 0.3, 0.6, 0.7)
+    """
+    return minimum(1, maximum(0, ((((max_value * ((domain - l_end) / (l_center - l_end))) * (domain <= l_center)) +
+                                   ((max_value * ((r_end - domain) / (r_end - r_center))) * (domain >= r_center))) +
+                                  (max_value * ((domain > l_center) * (domain < r_center))))))
