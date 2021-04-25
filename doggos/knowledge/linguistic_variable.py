827c4acc055e65c00e8e2a690dca9a4158f6d543
Adam Chlopowiec <adam.chlopowiec@op.pl>
2021-04-25 18:53:00 +0200

    Updated antecedent, linguistic variable and rule

--- doggos/knowledge/linguistic_variable.py
+++ doggos/knowledge/linguistic_variable.py
@@ -1,10 +1,33 @@
 from typing import Sequence, Tuple
 
 
-class LinguisticVariable:
+class Domain:
+    def __init__(self, domain: Sequence[Tuple[float, ...] or float], precision: float):
+        self.__domain = domain
+        self.__precision = precision
 
-    __name: str
-    __domain: Sequence[Tuple[float, ...] or float]
+    @property
+    def precision(self):
+        pass
+
+    @property
+    def domain(self):
+        pass
+
+    def min(self):
+        pass
+
+    def max(self):
+        pass
+
+
+class LinguisticVariable:
+    def __init__(self, name: str, domain: Domain):
+        self.__name = name
+        self.__domain = domain
 
     def __call__(self, x: float) -> Tuple[float, ...] or float:
         pass
+
+
+
