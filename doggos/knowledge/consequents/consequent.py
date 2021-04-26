68e69ad4b15bafd249100f9837efa75a90a4e0af
Adrian Chłopowiec <klopowik@gmail.com>
2021-04-26 18:04:28 +0200

    Added first implementation of MamdaniConsequent. Added docstrings describing class and methods

--- doggos/knowledge/consequents/consequent.py
+++ doggos/knowledge/consequents/consequent.py
@@ -1,9 +1,9 @@
 from abc import ABC, abstractmethod
-from typing import Tuple
+from typing import Tuple, List
 
 
 class Consequent(ABC):
 
     @abstractmethod
-    def calculate_cut(self) -> Tuple[float, ...] or float:
+    def calculate_cut(self, rule_firing: Tuple[float, ...] or float) -> Tuple[List[float]] or List[float]:
         pass
