330b2a13af7a7de0c230ffda44702546a262764a
Adam Chlopowiec <adam.chlopowiec@op.pl>
2021-04-26 21:08:12 +0200

    Added first version of karnik-mendel algorithm

--- doggos/inference/mamdani_inference_system.py
+++ doggos/inference/mamdani_inference_system.py
@@ -1,6 +1,7 @@
 from doggos.inference.inference_system import InferenceSystem
-from typing import List, Dict
+from typing import List, Dict, Sequence
 from doggos.knowledge import Rule
+from functools import partial
 import numpy as np
 
 
@@ -10,13 +11,14 @@ class MamdaniInferenceSystem(InferenceSystem):
 
     def calculate_output(self, features: Dict[str, float], method: str) -> float:
         rule_outputs = [rule.calculate_rule(features) for rule in self.__rule_base]
+        universe = self.__rule_base[0].domain.domain
 
         if method == "karnik_mendel":
             lmfs = [output[0] for output in rule_outputs]
             umfs = [output[0] for output in rule_outputs]
             lower_cut = self.__membership_func_union(lmfs)
             upper_cut = self.__membership_func_union(umfs)
-
+            return self.__karnik_mendel(lower_cut, upper_cut, universe)
 
     def __membership_func_union(self, mfs) -> np.ndarray:
         n_functions = len(mfs)
@@ -27,12 +29,34 @@ class MamdaniInferenceSystem(InferenceSystem):
         union = np.max(reshaped_mfs, axis=0)
         return union
 
+    def __karnik_mendel(self, lmf: np.ndarray, umf: np.ndarray, universe: Sequence[float]):
+        def find_k(c: float):
+            return np.where(universe_arr <= c <= universe_arr)
 
-    def __karnik_mendel(self, lmf: np.ndarray, umf: np.ndarray):
+        def find_c_minute(c: float, under_k_mf: np.ndarray, over_k_mf: np.ndarray):
+            k = find_k(c)
+            lower_thetas = under_k_mf[:(k + 1)]
+            upper_thetas = over_k_mf[(k + 1):]
+            weights = np.append(lower_thetas, upper_thetas)
+            return np.average(universe, weights=weights)
+
+        def find_y(partial_find_c_minute: partial):
+            c_prim = np.average(universe, weights=thetas)
+            c_minute = partial_find_c_minute(c_prim)
+            while abs(c_minute - c_prim) > np.finfo(float).eps:
+                c_prim = c_minute
+                c_minute, k = partial_find_c_minute(c_prim)
+            return c_minute
+
         n_functions = 2
         universe_size = lmf.size
+        universe_arr = np.array(universe, dtype=float)
         footprint = np.zeros(shape=(n_functions, universe_size))
         footprint[0] = lmf
         footprint[1] = umf
         thetas = np.sum(footprint, axis=0) / 2
-        universe = np.linspace(0, u)
+        y_l = find_y(partial(find_c_minute, under_k_mf=umf, over_k_mf=lmf))
+        y_r = find_y(partial(find_c_minute, under_k_mf=lmf, over_k_mf=umf))
+        return (y_l + y_r) / 2
+
+
