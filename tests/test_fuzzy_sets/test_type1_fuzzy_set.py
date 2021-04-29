7bf820b5324d7ebcbe561bd0d4846c30f7a015a4
Konrad Karanowski <konrad.karanowski@esportslab.gg>
2021-04-29 20:43:56 +0200

    Fixed

--- tests/test_fuzzy_sets/test_type1_fuzzy_set.py
+++ tests/test_fuzzy_sets/test_type1_fuzzy_set.py
@@ -6,6 +6,7 @@ from tests.test_fuzzy_sets import _random_sample
 
 from doggos.fuzzy_sets import Type1FuzzySet
 from doggos.utils.membership_functions import gaussian, sigmoid, triangular
+from doggos.fuzzy_sets.membership import MembershipDegreeT1
 
 
 class TestType1FuzzySet:
@@ -14,7 +15,7 @@ class TestType1FuzzySet:
     def test_base_fuzzyset(self, x):
         membership_function = gaussian(0, 1)
         fuzzy_set = Type1FuzzySet(membership_function)
-        assert fuzzy_set(x) == membership_function(x)
+        assert fuzzy_set(x) == MembershipDegreeT1(membership_function(x))
 
     def test_exception_uncallable_init(self):
         with pytest.raises(ValueError) as e:
@@ -32,7 +33,7 @@ class TestType1FuzzySet:
         membership_function2 = triangular(2, 5, 2, 1)
         fuzzy_set = Type1FuzzySet(membership_function1)
         fuzzy_set.membership_function = membership_function2
-        assert fuzzy_set(x) == membership_function2(x)
+        assert fuzzy_set(x) == MembershipDegreeT1(membership_function2(x))
 
     def test_exception_uncallable_setter(self):
         fuzzy_set = Type1FuzzySet(sigmoid(0, 1))
