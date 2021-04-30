664bacacbfab75172fa5454e27e2f022a3fec477
Primosz <valadin96@gmail.com>
2021-04-30 21:30:05 +0200

    Proposition - changed takagi_sugeno_consequent input values and parameters to dictionaries

--- doggos/knowledge/consequents/takagi_sugeno_consequent.py
+++ doggos/knowledge/consequents/takagi_sugeno_consequent.py
@@ -1,4 +1,4 @@
-from typing import List, Callable, NoReturn
+from typing import List, Callable, NoReturn, Dict
 
 from doggos.knowledge.consequents.consequent import Consequent
 
@@ -27,16 +27,17 @@ class TakagiSugenoConsequent(Consequent):
         out = ts.output([0.5, 0.9])
         """
 
-    def __init__(self, function_parameters: List[float]):
+    def __init__(self, function_parameters: Dict[str, float]):
         """
         Create Rules Consequent used in Takagi-Sugeno Inference System.
-        :param function_parameters: List[float] of parameters used for calculating output of consequent function
+        :param function_parameters: Dict[str, float] of input LinguisticVariable name and parameter used for calculating
+         output of consequent function
         """
         self.__function_parameters = function_parameters
         self.__consequent_output = 0
 
     @property
-    def function_parameters(self) -> List[float]:
+    def function_parameters(self) -> Dict[str, float]:
         """
         Getter of function parameters
         :return: function_parameters
@@ -44,29 +45,33 @@ class TakagiSugenoConsequent(Consequent):
         return self.__function_parameters
 
     @function_parameters.setter
-    def function_parameters(self, new_function_paramteres: List[float]) -> NoReturn:
+    def function_parameters(self, new_function_parameters: Dict[str, float]) -> NoReturn:
         """
         Sets new list of consequent's function parameters
-        :param new_function_paramteres: new list of consequent's function parameters
+        :param new_function_paramteres: new dictionary of consequent's function parameters
         :return: NoReturn
         """
-        if (not isinstance(new_function_paramteres, list) or
-                not all(isinstance(x, float) or isinstance(x, int) for x in new_function_paramteres)):
-            raise ValueError("Takagi-Sugeno consequent parameters must be list of floats!")
-        self.__function_parameters = new_function_paramteres
+        if (not isinstance(new_function_parameters, dict) or
+                not all(isinstance(x, float) or isinstance(x, int) for x in new_function_parameters.values()) or
+                not all(isinstance(x, str) for x in new_function_parameters.keys())):
+            raise ValueError("Takagi-Sugeno consequent parameters must be Dict[str, float]!")
+        self.__function_parameters = new_function_parameters
 
-    def output(self, consequent_input: List[float]) -> float:
+    def output(self, consequent_input: Dict[str, float]) -> float:
         """
         Return rule output level by calculating consequent function with inputs as variables.
-        :param consequent_input: inputs of the inference system
+        :param consequent_input: inputs of the inference system in Dict[str, float], which reflects input linguistic
+                variable name and value
                 IMPORTANT: Number of inputs must be one less than number of function parameters!
+                           Last parameter should have key 'const' as it reflects form of output function.
         :return: crisp rule output value that needs to be used in aggregation process
         """
         self.__consequent_output = 0
-        if len(consequent_input) == len(self.__function_parameters) - 1:
-            for idx, inp in enumerate(consequent_input):
-                self.__consequent_output += self.__function_parameters[idx] * consequent_input[idx]
-            self.__consequent_output += self.__function_parameters[-1]
+        if all(key in self.__function_parameters.keys() for key in list(consequent_input.keys()) + ['const']):
+            for key in consequent_input:
+                self.__consequent_output += consequent_input[key] * self.__function_parameters[key]
+            if 'const' in self.__function_parameters.keys():
+                self.__consequent_output += self.__function_parameters['const']
             return self.__consequent_output
         else:
-            raise Exception("Number of inputs must be one less than number of consequent parameters!")
+            raise Exception("Function parameters contain value for input which was not provided!")
--- tests/test_knowledge/test_consequents/test_takagi_sugeno_consequent.py
+++ tests/test_knowledge/test_consequents/test_takagi_sugeno_consequent.py
@@ -5,45 +5,45 @@ import pytest
 class TestTakagiSugenoConsequent:
 
     def test_output_1_input(self):
-        ts1 = TakagiSugenoConsequent([0.1, -2.5])
-        assert (ts1.output([1]) == -2.4)
-        assert (ts1.output([0]) == -2.5)
+        ts1 = TakagiSugenoConsequent({'F1': 0.1, 'const': -2.5})
+        assert (ts1.output({'F1': 1}) == -2.4)
+        assert (ts1.output({'F1': 0}) == -2.5)
 
     def test_output_2_inputs(self):
-        ts2 = TakagiSugenoConsequent([2, 10, 1])
-        assert (ts2.output([1, 1]) == 13)
-        assert (ts2.output([0, 0]) == 1)
+        ts2 = TakagiSugenoConsequent({'F1': 2, 'F2': 10, 'const': 1})
+        assert (ts2.output({'F1': 1, 'F2': 1}) == 13)
+        assert (ts2.output({'F1': 0, 'F2': 0}) == 1)
 
     def test_output_5_inputs(self):
-        ts5 = TakagiSugenoConsequent([1.5, 6.123, 2.5, -2, 0.15, 0.99])
-        assert (ts5.output([1, 0.1, 2, 0.2, 3]) == 8.1523)  #(1.5 * 1) + (6.123 * 0.1) + (2.5 * 2) + (-2 * 0.2) + (0.15 * 3) + 0.99
-        assert (ts5.output([0, 0, 0, 0, 1]) == 1.14)
+        ts5 = TakagiSugenoConsequent({'F1': 1.5, 'F2': 6.123, 'F3': 2.5, 'F4': -2, 'F5':  0.15, 'const': 0.99})
+        assert (ts5.output({'F1': 1, 'F2': 0.1, 'F3': 2, 'F4': 0.2, 'F5': 3}) == 8.1523)  #(1.5 * 1) + (6.123 * 0.1) + (2.5 * 2) + (-2 * 0.2) + (0.15 * 3) + 0.99
+        assert (ts5.output({'F1': 0, 'F2': 0, 'F3': 0, 'F4': 0, 'F5': 1}) == 1.14)
 
     def test_wrong_inputs(self):
-        ts = TakagiSugenoConsequent([1, 2, 3, 4])
+        ts = TakagiSugenoConsequent({'F1': 1, 'F2': 2, 'F3': 3, 'const': 4})
         with pytest.raises(Exception) as e:
-            ts.output([1, 2])
+            ts.output({'F1': 1, 'F2': 2})
             assert "Number of inputs must be one less than number of consequent parameters!" in str(e.value)
 
         with pytest.raises(Exception) as e:
-            ts.output([1, 2, 3, 4])
+            ts.output({'F1': 1, 'F2': 2, 'F3': 3, 'F4': 4})
             assert "Number of inputs must be one less than number of consequent parameters!" in str(e.value)
 
     def test_setter(self):
-        tss = TakagiSugenoConsequent([1, 2, 3])
-        assert (tss.output([0.5, 0.2]) == 3.9)
-        tss.function_parameters = [0.1, 0.1, 1]
-        assert (tss.output([0.99, 0.88]) == 1.187)
+        tss = TakagiSugenoConsequent({'F1': 1, 'F2': 2, 'const': 3})
+        assert (tss.output({'F1': 0.5, 'F2': 0.2}) == 3.9)
+        tss.function_parameters = {'F1': 0.1, 'F2': 0.1, 'const': 1}
+        assert (tss.output({'F1': 0.99, 'F2': 0.88}) == 1.187)
 
     def test_setter_error(self):
-        tse = TakagiSugenoConsequent([0.2, -1, 0.5])
-        assert (tse.output([0.5, 0.2]) == 0.4)
+        tse = TakagiSugenoConsequent({'F1': 0.2, 'F2': -1, 'const': 0.5})
+        assert (tse.output({'F1': 0.5, 'F2': 0.2}) == 0.4)
         with pytest.raises(ValueError) as e:
-            tse.function_parameters = [1.0, -5, 'a']
-            assert "Takagi-Sugeno consequent parameters must be list of floats!" in str(e.value)
+            tse.function_parameters = {'F1': 1.0, 'F2': -5, 'const': 'a'}
+            assert "Takagi-Sugeno consequent parameters must be Dict[str, float]!" in str(e.value)
 
     def test_getter(self):
-        tsg = TakagiSugenoConsequent([1, 2, 3])
-        assert (tsg.function_parameters == [1, 2, 3])
-        tsg.function_parameters = [0.1, 0.1, 1]
-        assert (tsg.function_parameters == [0.1, 0.1, 1])
+        tsg = TakagiSugenoConsequent({'F1': 1, 'F2': 2, 'const': 3})
+        assert (tsg.function_parameters == {'F1': 1, 'F2': 2, 'const': 3})
+        tsg.function_parameters = {'F1': 0.1, 'F2': 0.1, 'const': 1}
+        assert (tsg.function_parameters == {'F1': 0.1, 'F2': 0.1, 'const': 1})
