519208ee7350ed396035e30cccca11fc3ca21a54
chrisgal77 <krzysztof.galus.k7@gmail.com>
2021-04-30 14:17:31 +0200

    setters and getters

--- doggos/knowledge/antecedent.py
+++ doggos/knowledge/antecedent.py
@@ -1,4 +1,4 @@
-from typing import Dict, Sequence
+from typing import Dict, Sequence, Callable, NoReturn
 from doggos.algebras.algebra import Algebra
 from doggos.fuzzy_sets.membership.membership_degree import MembershipDegree
 from doggos.knowledge import Clause
@@ -9,36 +9,50 @@ class Antecedent(ABC):
     """
     Class representing antecedent:
     https://en.wikipedia.org/wiki/Fuzzy_set
-
-    Attributes
-    --------------------------------------------
-    __clauses : Sequence[Clause]
-        sequence of clauses
-    __algebra : Algebra
-        algebra provides t-norm and s-norm
-
-    Methods
-    --------------------------------------------
-    firing(self, features: Dict[str, float]) -> MembershipDegree:
-        returns
     
     """
-    def __init__(self, clauses: Sequence[Clause], algebra: Algebra):
-        pass
-
-    def firing(self, features: Dict[Clause, MembershipDegree]) -> MembershipDegree:
-        pass
+    __algebra: Algebra
+    __fire: Callable[[Dict[Clause, MembershipDegree]], MembershipDegree]
 
 
 class TermAntecedent(Antecedent):
     
-    def __init__(self, clause: Clause, algebra: Algebra):
+    def __init__(self, algebra: Algebra, clause: Clause = None):
         self.__clause = clause
         self.__algebra = algebra
-        self.fire = lambda dict_: dict_[clause]
+        self.__fire = lambda dict_: dict_[clause]
 
-    def __and__(self, clause: Clause) -> TermAntecedent:
-        return self.__class__()
+    def __and__(self, term: TermAntecedent) -> TermAntecedent:
+        rterm = self.__class__(self.algebra)
+        rterm.fire = lambda dict_: self.algebra.t_norm(self.fire(dict_), term.fire(dict_))
+        return rterm
     
-    def __or__(self, clause: Clause) -> TermAntecedent:
-        pass
+    def __or__(self, term: TermAntecedent) -> TermAntecedent:
+        rterm = self.__class__(self.algebra)
+        rterm.fire = lambda dict_: self.algebra.s_norm(self.fire(dict_), term.fire(dict_))
+        return rterm
+
+    @property
+    def clause(self) -> Clause:
+        return self.__clause
+
+    @clause.setter
+    def clause(self, clause: Clause) -> NoReturn:
+        self.__clause = clause
+
+    @property
+    def fire(self) -> Callable[[Dict[Clause, MembershipDegree]], MembershipDegree]:
+        return self.__fire
+
+    @fire.setter
+    def fire(self, fire: Callable[[Dict[Clause, MembershipDegree]], MembershipDegree]) -> NoReturn:
+        self.__fire = fire
+
+    @property
+    def algebra(self) -> Algebra:
+        return self.__algebra
+
+    @algebra.setter
+    def algebra(self, algebra: Algebra):
+        self.__algebra = algebra
+
