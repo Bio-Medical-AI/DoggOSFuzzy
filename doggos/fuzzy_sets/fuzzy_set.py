9fb53be25b6ec5f06150117c1f2380cb61b772ad
Adrian Chłopowiec <klopowik@gmail.com>
2021-05-01 23:16:28 +0200

    Added docstring for MembershipDegree

--- doggos/fuzzy_sets/fuzzy_set.py
+++ doggos/fuzzy_sets/fuzzy_set.py
@@ -3,6 +3,15 @@ from typing import NewType
 
 
 MembershipDegree = NewType('MembershipDegree', None)
+"""
+MembershipDegree is a type hint for membership degree of some value to a certain fuzzy set. Membership degree type
+varies depending on type of fuzzy set used. Therefore we use one type hint for all types for fuzzy sets, so treat it
+only as a hint, its not a type.
+E.g.:
+For type one fuzzy set membership degree is a float.
+For type two fuzzy set membership degree is a Tuple[float, float]
+For other types of fuzzy sets is a membership degree defined by user.
+"""
 
 
 class FuzzySet(ABC):
