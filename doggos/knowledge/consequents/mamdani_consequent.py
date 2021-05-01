f78ca06126b37bb6c9bd7599ea8a09400caeceb2
Adrian Chłopowiec <klopowik@gmail.com>
2021-05-01 23:04:53 +0200

    Changed MembershipDegree type hint import

--- doggos/knowledge/consequents/mamdani_consequent.py
+++ doggos/knowledge/consequents/mamdani_consequent.py
@@ -3,12 +3,12 @@ import numpy as np
 
 from doggos.knowledge.consequents.consequent import Consequent
 from doggos.knowledge.clause import Clause
-from doggos.fuzzy_sets.membership.membership_degree import MembershipDegree
+from doggos.fuzzy_sets import MembershipDegree
 
 
 class MamdaniConsequent(Consequent):
     """
-    Class used to represent a fuzzy rule consequent:
+    Class used to represent a fuzzy rule Mamdani consequent:
     https://en.wikipedia.org/wiki/Fuzzy_rule
 
     Attributes
@@ -17,7 +17,7 @@ class MamdaniConsequent(Consequent):
         supplies Consequent with universe and a fuzzy set. Provided fuzzy set type must match fuzzy sets used in Fuzzy
         Rule Antecedent, otherwise an exception will be raised during computations.
 
-    __cut_mf : Tuple[List[float], ...] or List[float]
+    __cut_mf : List[MembershipDegree]
         fuzzy set provided by clause cut to the rule firing level
 
     Methods
@@ -49,24 +49,24 @@ class MamdaniConsequent(Consequent):
         :param rule_firing: firing value of a Rule in which Consequent is used
         :return: fuzzy set membership function or tuple of those, cut to the level of firing value
         """
-        if isinstance(rule_firing, float):
+        if isinstance(rule_firing, MembershipDegreeT1):
             return self.__t1_cut(rule_firing)
-        elif isinstance(rule_firing, tuple):
-            if len(rule_firing) == 2:
-                return self.__it2_cut(rule_firing)
+        elif isinstance(rule_firing, MembershipDegreeIT2):
+            return self.__it2_cut(rule_firing)
         else:
-            raise Exception
+            raise ValueError(f"Incorrect type of rule firing: {rule_firing}")
 
-    def __t1_cut(self, rule_firing: float):
+    def __t1_cut(self, rule_firing: MembershipDegreeT1):
         """
         Makes a cut for type one fuzzy sets. If Clause fuzzy set type mismatches rule_firing type, exception is raised.
         :param rule_firing: crisp value of rule firing
         :return: fuzzy set membership function, cut to the level of firing value
         """
-        self.__cut_mf = np.minimum(self.__clause.fuzzy_set, rule_firing)
+        self.__cut_mf = [membership if membership.value <= rule_firing.value else clone(rule_firing)
+                         for membership in self.__clause.values]
         return self.__cut_mf
 
-    def __it2_cut(self, rule_firing: Tuple[float, float]):
+    def __it2_cut(self, rule_firing: MembershipDegreeIT2):
         """
         Makes a cut for interval type two fuzzy sets. If Clause fuzzy set type mismatches rule_firing type, exception is
         raised. Lower membership function is cut to level of first element of tuple, upper membership function is cut to
@@ -74,6 +74,6 @@ class MamdaniConsequent(Consequent):
         :param rule_firing: tuple of crisp values of rule firing
         :return: tuple of fuzzy set membership functions, cut to the level of firing value
         """
-        self.__cut_mf = (np.minimum(self.__clause.fuzzy_set[0], rule_firing[0]),
-                         np.minimum(self.__clause.fuzzy_set[1], rule_firing[1]))
+        self.__cut_mf = (np.minimum(self.__clause.values, rule_firing.value[0]),
+                         np.minimum(self.__clause.values, rule_firing.value[0]))
         return self.__cut_mf
