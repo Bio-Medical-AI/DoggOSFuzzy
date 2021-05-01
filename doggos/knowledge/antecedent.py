56b93583954e52aec4e3b0feb607bef6e9f38bda
chrisgal77 <krzysztof.galus.k7@gmail.com>
2021-05-01 14:01:54 +0200

    changed fire to method

--- doggos/knowledge/antecedent.py
+++ doggos/knowledge/antecedent.py
@@ -5,7 +5,7 @@ from doggos.algebras.algebra import Algebra
 from doggos.fuzzy_sets.membership.membership_degree import MembershipDegree
 from doggos.knowledge.clause import Clause
 
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class Antecedent(ABC):
@@ -14,6 +14,9 @@ class Antecedent(ABC):
     https://en.wikipedia.org/wiki/Fuzzy_set
     
     """
+    @abstractmethod
+    def fire(self, clause_dict: Dict[Clause, MembershipDegree]) -> MembershipDegree:
+        pass
 
 
 class Term(Antecedent):
@@ -30,7 +33,9 @@ class Term(Antecedent):
             raise TypeError('algebra must be a Algebra type')
         self.__clause = clause
         self.__algebra = algebra
-        self.fire = lambda dict_: dict_[clause]
+        
+    def fire(self, clause_dict: Dict[Clause, MembershipDegree]) -> MembershipDegree:
+        return clause_dict[self.clause]
 
     def __and__(self, term: Term) -> Term:
         new_term = self.__class__(self.algebra)
