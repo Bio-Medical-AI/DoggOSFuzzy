4ed62383480ec77fad7e408644ab6b4e2e18b511
Konrad Karanowski <konrad.karanowski@esportslab.gg>
2021-05-02 00:15:01 +0200

    Fixed test

--- tests/test_algebras/test_lukasiewicz_algebra.py
+++ tests/test_algebras/test_lukasiewicz_algebra.py
@@ -1,31 +1,28 @@
-import pytest
-
-
-from tests.test_config import TestConfig
+from tests.test_tools import approx
 from doggos.algebras import LukasiewiczAlgebra
 
 
 class TestLukasiewiczAlgebra:
 
     def test_t_norm(self):
-        assert LukasiewiczAlgebra.t_norm(0.9, 0.2) == pytest.approx(0.1, TestConfig.FLOAT_COMPARISON_PRECISION)
-        assert LukasiewiczAlgebra.t_norm(1.0, 1.0) == pytest.approx(1.0, TestConfig.FLOAT_COMPARISON_PRECISION)
-        assert LukasiewiczAlgebra.t_norm(1.0, 0.0) == pytest.approx(0.0, TestConfig.FLOAT_COMPARISON_PRECISION)
-        assert LukasiewiczAlgebra.t_norm(0.2, 0.35) == pytest.approx(0.0, TestConfig.FLOAT_COMPARISON_PRECISION)
+        assert LukasiewiczAlgebra.t_norm(0.9, 0.2) == approx(0.1)
+        assert LukasiewiczAlgebra.t_norm(1.0, 1.0) == approx(1.0)
+        assert LukasiewiczAlgebra.t_norm(1.0, 0.0) == approx(0.0)
+        assert LukasiewiczAlgebra.t_norm(0.2, 0.35) == approx(0.0)
 
     def test_s_norm(self):
-        assert LukasiewiczAlgebra.s_norm(0.9, 0.2) == pytest.approx(1.0, TestConfig.FLOAT_COMPARISON_PRECISION)
-        assert LukasiewiczAlgebra.s_norm(1.0, 1.0) == pytest.approx(1.0, TestConfig.FLOAT_COMPARISON_PRECISION)
-        assert LukasiewiczAlgebra.s_norm(1.0, 0.0) == pytest.approx(1.0, TestConfig.FLOAT_COMPARISON_PRECISION)
-        assert LukasiewiczAlgebra.s_norm(0.2, 0.35) == pytest.approx(0.55, TestConfig.FLOAT_COMPARISON_PRECISION)
+        assert LukasiewiczAlgebra.s_norm(0.9, 0.2) == approx(1.0)
+        assert LukasiewiczAlgebra.s_norm(1.0, 1.0) == approx(1.0)
+        assert LukasiewiczAlgebra.s_norm(1.0, 0.0) == approx(1.0)
+        assert LukasiewiczAlgebra.s_norm(0.2, 0.35) == approx(0.55)
 
     def test_negation(self):
-        assert LukasiewiczAlgebra.negation(1.0) == pytest.approx(0.0, TestConfig.FLOAT_COMPARISON_PRECISION)
-        assert LukasiewiczAlgebra.negation(0.0) == pytest.approx(1.0, TestConfig.FLOAT_COMPARISON_PRECISION)
-        assert LukasiewiczAlgebra.negation(0.2) == pytest.approx(0.8, TestConfig.FLOAT_COMPARISON_PRECISION)
+        assert LukasiewiczAlgebra.negation(1.0) == approx(0.0)
+        assert LukasiewiczAlgebra.negation(0.0) == approx(1.0)
+        assert LukasiewiczAlgebra.negation(0.2) == approx(0.8)
 
     def test_implication(self):
-        assert LukasiewiczAlgebra.implication(1.0, 0.0) == pytest.approx(0.0, TestConfig.FLOAT_COMPARISON_PRECISION)
-        assert LukasiewiczAlgebra.implication(0.0, 1.0) == pytest.approx(1.0, TestConfig.FLOAT_COMPARISON_PRECISION)
-        assert LukasiewiczAlgebra.implication(0.2, 0.35) == pytest.approx(1.0, TestConfig.FLOAT_COMPARISON_PRECISION)
-        assert LukasiewiczAlgebra.implication(0.95, 0.2) == pytest.approx(0.25, TestConfig.FLOAT_COMPARISON_PRECISION)
+        assert LukasiewiczAlgebra.implication(1.0, 0.0) == approx(0.0)
+        assert LukasiewiczAlgebra.implication(0.0, 1.0) == approx(1.0)
+        assert LukasiewiczAlgebra.implication(0.2, 0.35) == approx(1.0)
+        assert LukasiewiczAlgebra.implication(0.95, 0.2) == approx(0.25)
--- tests/test_knowledge/test_antecedent.py
+++ tests/test_knowledge/test_antecedent.py
@@ -1,24 +1,21 @@
 import pytest
 
 from doggos.algebras import GodelAlgebra
-from doggos.knowledge import LinguisticVariable, Domain
 from doggos.knowledge import Clause
 from doggos.knowledge import Term
 from doggos.fuzzy_sets import Type1FuzzySet
 from doggos.knowledge.linguistic_variable import LinguisticVariable, Domain
 
-import numpy as np
-
 
 class TestTermAntecedent:
 
-    def test_exception_typerror_algebra_init(self):
+    def test_exception_typeerror_algebra_init(self):
         domain = Domain(0, 10, 0.5)
         ling_var = LinguisticVariable('Temperature', domain)
-        fuzzy_set = Type1FuzzySet(lambda x: 0.5*x)
+        fuzzy_set = Type1FuzzySet(lambda x: 0.5 * x)
         clause = Clause(ling_var, 'Low', fuzzy_set)
         with pytest.raises(TypeError) as e:
-            clause = Term([], clause)
+            _ = Term([], clause)
             assert 'algebra must be an Algebra type' in str(e.value)
 
     def test_and(self):
@@ -27,7 +24,7 @@ class TestTermAntecedent:
         ling_var = LinguisticVariable('Temperature', domain)
         fuzzy_set1 = Type1FuzzySet(lambda x: 0.5 * x)
         clause1 = Clause(ling_var, 'Low', fuzzy_set1)
-        fuzzy_set2 = Type1FuzzySet(lambda x:2*x)
+        fuzzy_set2 = Type1FuzzySet(lambda x: 2 * x)
         clause2 = Clause(ling_var, 'High', fuzzy_set2)
         clause_dict = {
             clause1: clause1.get_value(2),
@@ -44,7 +41,7 @@ class TestTermAntecedent:
         ling_var = LinguisticVariable('Temperature', domain)
         fuzzy_set1 = Type1FuzzySet(lambda x: 0.5 * x)
         clause1 = Clause(ling_var, 'Low', fuzzy_set1)
-        fuzzy_set2 = Type1FuzzySet(lambda x:2*x)
+        fuzzy_set2 = Type1FuzzySet(lambda x: 2 * x)
         clause2 = Clause(ling_var, 'High', fuzzy_set2)
         clause_dict = {
             clause1: clause1.get_value(2),
@@ -55,17 +52,17 @@ class TestTermAntecedent:
         antecedent = antecedent1 | antecedent2
         assert antecedent.fire(clause_dict) == algebra.s_norm(clause_dict[clause1], clause_dict[clause2])
 
-    def test_complex(self):  
+    def test_complex(self):
         algebra = GodelAlgebra()
         domain = Domain(0, 10, 0.5)
         ling_var = LinguisticVariable('Temperature', domain)
         fuzzy_set1 = Type1FuzzySet(lambda x: 0.5 * x)
         clause1 = Clause(ling_var, 'Low', fuzzy_set1)
-        fuzzy_set2 = Type1FuzzySet(lambda x: 2*x)
+        fuzzy_set2 = Type1FuzzySet(lambda x: 2 * x)
         clause2 = Clause(ling_var, 'High', fuzzy_set2)
-        fuzzy_set3 = Type1FuzzySet(lambda x: 4*x)
+        fuzzy_set3 = Type1FuzzySet(lambda x: 4 * x)
         clause3 = Clause(ling_var, 'Giga High', fuzzy_set2)
-        fuzzy_set4 = Type1FuzzySet(lambda x: 3*x)
+        fuzzy_set4 = Type1FuzzySet(lambda x: 3 * x)
         clause4 = Clause(ling_var, 'Very High', fuzzy_set2)
         clause_dict = {
             clause1: clause1.get_value(2),
@@ -78,5 +75,6 @@ class TestTermAntecedent:
         antecedent3 = Term(algebra, clause3)
         antecedent4 = Term(algebra, clause4)
         antecedent = (antecedent1 | antecedent2) & (antecedent3 & antecedent4)
-        assert antecedent.fire(clause_dict) == algebra.t_norm(algebra.s_norm(clause_dict[clause1], clause_dict[clause2]), \
-                                                              algebra.t_norm(clause_dict[clause3], clause_dict[clause4]))
+        assert antecedent.fire(clause_dict) == algebra.t_norm(
+            algebra.s_norm(clause_dict[clause1], clause_dict[clause2]),
+            algebra.t_norm(clause_dict[clause3], clause_dict[clause4]))
