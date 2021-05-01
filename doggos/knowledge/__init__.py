94cebd70cf066fec535ec7187c0fe8edfd65a571
chrisgal77 <krzysztof.galus.k7@gmail.com>
2021-05-01 14:46:58 +0200

    added term to __init__

--- doggos/knowledge/__init__.py
+++ doggos/knowledge/__init__.py
@@ -1,6 +1,7 @@
 from doggos.knowledge.linguistic_variable import LinguisticVariable, Domain
 from doggos.knowledge.clause import Clause
-from doggos.knowledge.antecedent import Antecedent, Term
+from doggos.knowledge.antecedent import Antecedent
+from doggos.knowledge.term import Term
 from doggos.knowledge.rule import Rule
 
 
