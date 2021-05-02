4bf12c17b50c496a71bcce18c97ebb89ec3f9c8c
Primosz <valadin96@gmail.com>
2021-05-02 13:44:03 +0200

    Adjusted tests

--- tests/test_knowledge/test_consequents/test_takagi_sugeno_consequent.py
+++ tests/test_knowledge/test_consequents/test_takagi_sugeno_consequent.py
@@ -1,3 +1,4 @@
+from doggos.knowledge import Domain, LinguisticVariable
 from doggos.knowledge.consequents import TakagiSugenoConsequent
 import pytest
 
@@ -5,39 +6,62 @@ import pytest
 class TestTakagiSugenoConsequent:
 
     def test_output_1_input(self):
+        domain = Domain(0, 10, 0.01)
+        ling_var_f1 = LinguisticVariable('F1', domain)
+
         ts1 = TakagiSugenoConsequent({'F1': 0.1, 'const': -2.5})
-        assert (ts1.output({'F1': 1}) == -2.4)
-        assert (ts1.output({'F1': 0}) == -2.5)
+        assert (ts1.output({ling_var_f1: 1}) == -2.4)
+        assert (ts1.output({ling_var_f1: 0}) == -2.5)
 
     def test_output_2_inputs(self):
+        domain = Domain(0, 10, 0.01)
+        ling_var_f1 = LinguisticVariable('F1', domain)
+        ling_var_f2 = LinguisticVariable('F2', domain)
+
         ts2 = TakagiSugenoConsequent({'F1': 2, 'F2': 10, 'const': 1})
-        assert (ts2.output({'F1': 1, 'F2': 1}) == 13)
-        assert (ts2.output({'F1': 0, 'F2': 0}) == 1)
+        assert (ts2.output({ling_var_f1: 1, ling_var_f2: 1}) == 13)
+        assert (ts2.output({ling_var_f1: 0, ling_var_f2: 0}) == 1)
 
     def test_output_5_inputs(self):
-        ts5 = TakagiSugenoConsequent({'F1': 1.5, 'F2': 6.123, 'F3': 2.5, 'F4': -2, 'F5':  0.15, 'const': 0.99})
-        assert (ts5.output({'F1': 1, 'F2': 0.1, 'F3': 2, 'F4': 0.2, 'F5': 3}) == 8.1523)  #(1.5 * 1) + (6.123 * 0.1) + (2.5 * 2) + (-2 * 0.2) + (0.15 * 3) + 0.99
-        assert (ts5.output({'F1': 0, 'F2': 0, 'F3': 0, 'F4': 0, 'F5': 1}) == 1.14)
+        domain = Domain(0, 10, 0.01)
+        ling_var_f1 = LinguisticVariable('F1', domain)
+        ling_var_f2 = LinguisticVariable('F2', domain)
+        ling_var_f3 = LinguisticVariable('F3', domain)
+        ling_var_f4 = LinguisticVariable('F4', domain)
+        ling_var_f5 = LinguisticVariable('F5', domain)
+
+        ts5 = TakagiSugenoConsequent({'F1': 1.5, 'F2': 6.123, 'F3': 2.5, 'F4': -2, 'F5': 0.15, 'const': 0.99})
+        assert (ts5.output({ling_var_f1: 1, ling_var_f2: 0.1, ling_var_f3: 2, ling_var_f4: 0.2, ling_var_f5: 3}) ==
+                8.1523)  # (1.5 * 1) + (6.123 * 0.1) + (2.5 * 2) + (-2 * 0.2) + (0.15 * 3) + 0.99
+        assert (ts5.output({ling_var_f1: 0, ling_var_f2: 0, ling_var_f3: 0, ling_var_f4: 0, ling_var_f5: 1}) == 1.14)
 
     def test_wrong_inputs(self):
+        domain = Domain(0, 10, 0.01)
+        ling_var_f1 = LinguisticVariable('F1', domain)
+        ling_var_f2 = LinguisticVariable('F2', domain)
+
         ts = TakagiSugenoConsequent({'F1': 1, 'F2': 2, 'F3': 3, 'const': 4})
         with pytest.raises(Exception) as e:
-            ts.output({'F1': 1, 'F2': 2})
-            assert "Number of inputs must be one less than number of consequent parameters!" in str(e.value)
-
-        with pytest.raises(Exception) as e:
-            ts.output({'F1': 1, 'F2': 2, 'F3': 3, 'F4': 4})
-            assert "Number of inputs must be one less than number of consequent parameters!" in str(e.value)
+            ts.output({ling_var_f1: 1, ling_var_f2: 2})
+            assert "Function parameters contain value for input which was not provided!" in str(e.value)
 
     def test_setter(self):
+        domain = Domain(0, 10, 0.01)
+        ling_var_f1 = LinguisticVariable('F1', domain)
+        ling_var_f2 = LinguisticVariable('F2', domain)
+
         tss = TakagiSugenoConsequent({'F1': 1, 'F2': 2, 'const': 3})
-        assert (tss.output({'F1': 0.5, 'F2': 0.2}) == 3.9)
+        assert (tss.output({ling_var_f1: 0.5, ling_var_f2: 0.2}) == 3.9)
         tss.function_parameters = {'F1': 0.1, 'F2': 0.1, 'const': 1}
-        assert (tss.output({'F1': 0.99, 'F2': 0.88}) == 1.187)
+        assert (tss.output({ling_var_f1: 0.99, ling_var_f2: 0.88}) == 1.187)
 
     def test_setter_error(self):
+        domain = Domain(0, 10, 0.01)
+        ling_var_f1 = LinguisticVariable('F1', domain)
+        ling_var_f2 = LinguisticVariable('F2', domain)
+
         tse = TakagiSugenoConsequent({'F1': 0.2, 'F2': -1, 'const': 0.5})
-        assert (tse.output({'F1': 0.5, 'F2': 0.2}) == 0.4)
+        assert (tse.output({ling_var_f1: 0.5, ling_var_f2: 0.2}) == 0.4)
         with pytest.raises(ValueError) as e:
             tse.function_parameters = {'F1': 1.0, 'F2': -5, 'const': 'a'}
             assert "Takagi-Sugeno consequent parameters must be Dict[str, float]!" in str(e.value)
