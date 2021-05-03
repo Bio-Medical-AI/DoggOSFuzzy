08d15d64c79f4d0dcb9b0028cc1ce2c5ec9e1ea0
Konrad Karanowski <konrad.karanowski@esportslab.gg>
2021-05-03 16:41:46 +0200

    Adjusted algebra and fuzzy sets

--- doggos/fuzzy_sets/fuzzy_set.py
+++ doggos/fuzzy_sets/fuzzy_set.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import NewType, Iterable
 
-
 MembershipDegree = NewType('MembershipDegree', None)
 """
 MembershipDegree is a type hint for membership degree of some value to a certain fuzzy set. Membership degree type
@@ -19,4 +18,3 @@ class FuzzySet(ABC):
     @abstractmethod
     def __call__(self, x: float or Iterable[float]) -> MembershipDegree:
         pass
-
