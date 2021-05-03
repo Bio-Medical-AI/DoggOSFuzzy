396db481ff0ebbdea17a6fe1218dcc7896c25dbe
Konrad Karanowski <konrad.karanowski@esportslab.gg>
2021-05-03 23:42:20 +0200

    Now it works xDDDDDDDDDDDD

--- doggos/inference/mamdani_inference_system.py
+++ doggos/inference/mamdani_inference_system.py
@@ -65,28 +65,28 @@ class MamdaniInferenceSystem(InferenceSystem):
 
         degrees = self.__get_degrees(features)
         is_type1 = self.__is_consequent_type1()
-        result = np.zeros(shape=(1, len(degrees)))
+        result = np.zeros(shape=(1, degrees))
 
-        for i in range(len(degrees)):
+        for i in range(degrees):
             single_features = {}
             for clause, memberships in features.items():
-                single_features[clause] = memberships[i]
+                single_features[clause] = np.take(memberships, i, axis=-1)
 
             if is_type1:
                 domain, membership_functions = self.__get_domain_and_consequents_membership_functions(single_features)
                 result[i] = defuzzification_method(domain, membership_functions)
             else:
                 domain, lmfs, umfs = self.__get_domain_and_consequents_memberships_for_it2(single_features)
-                result[i] = defuzzification_method(lmfs, umfs, domain)
+                result[:, i] = defuzzification_method(lmfs, umfs, domain)
 
         if result.shape[1] == 1:
             return result.item()
 
-        return result
+        return np.squeeze(result, 0)
 
     def __get_degrees(self, features: Dict[Clause, List[MembershipDegree]]) -> List[MembershipDegree]:
         values = np.array(list(features.values()))
-        return values[0]
+        return values[0].shape[1]
 
     def __validate_consequents(self) -> NoReturn:
         for rule in self._rule_base:
