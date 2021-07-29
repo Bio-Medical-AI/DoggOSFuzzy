79cf791eaa799357ee7d97afe7011ebdc30c0fbb
Adam Chlopowiec <adam.chlopowiec@op.pl>
2021-07-29 14:59:15 +0200

    Fixed bugs, refactored inconsistencies remover code, documented __solve_conflicts method

--- doggos/induction/rule_induction_WIP/fuzzy_decision_table_generator.py
+++ doggos/induction/rule_induction_WIP/fuzzy_decision_table_generator.py
@@ -1,3 +1,5 @@
+import sys
+
 import pandas as pd
 from typing import Dict, List
 from doggos.knowledge import Clause, LinguisticVariable, Domain
@@ -44,27 +46,28 @@ class FuzzyDecisionTableGenerator:
         """
         self.__fuzzy_sets = fuzzy_sets
         self.__dataset = X
-        self.__dataset['Decision'] = y
         for column in self.__dataset.columns:
             self.__dataset.rename({column: str(column)})
 
         self.__features = [LinguisticVariable(feature_name, Domain(0, 1.001, 0.001))
                            for feature_name in self.__dataset.columns]
         self.__features_clauses = {feature_name: [] for feature_name in self.__dataset.columns}
+        self.__dataset['Decision'] = y
 
-    def __get_highest_membership(self, feature: str, input: float) -> str:
+    def __get_highest_membership(self, feature: str, crisp: float) -> str:
         """
         Determines a name of a fuzzy set with the highest membership value for a given feature
         :param feature: a name of a feature for which to determine a name of the highest membership set
-        :param input: a crisp value of the feature
+        :param crisp: a crisp value of the feature
         :return: a name of a fuzzy set with the highest member value for a given feature
         """
         max_feature = ''
-        max_value = 0
+        max_value = -sys.maxsize
         for clause in self.__features_clauses[feature]:
-            if clause.get_value(input) > max_value:
+            if clause.get_value(crisp) > max_value:
                 max_feature = clause.gradation_adjective
-                max_value = clause.get_value(input)
+                max_value = clause.get_value(crisp)
+
         return max_feature
 
     def __fuzzify_dataset(self) -> pd.DataFrame:
@@ -90,7 +93,7 @@ class FuzzyDecisionTableGenerator:
         :return: dataset with crisp values replaced by names of highest membership fuzzy sets
         """
         for feature in self.__features:
-            if feature.name is not 'Decision':
+            if feature.name != 'Decision':
                 self.__features_clauses[feature.name] = []
                 for key in self.__fuzzy_sets[feature.name].keys():
                     self.__features_clauses[feature.name].append(Clause(feature,
--- doggos/induction/rule_induction_WIP/inconsistencies_remover.py
+++ doggos/induction/rule_induction_WIP/inconsistencies_remover.py
@@ -51,13 +51,13 @@ class InconsistenciesRemover(object):
             # on rows. Numpy.where returns 2-elem tuple with second element being empty, thus 0-th is taken.
             decision_indices.append(np.where((left == right).all(-1))[0].item())
 
-        decisions = [features_decisions_occurrence['Decision'].values[idx] for idx in decision_indices]
+        decisions = [feature_decision_single_occurrences['Decision'].values[idx] for idx in decision_indices]
         feature_sets_single_occurrences['Decision'] = decisions
 
         return feature_sets_single_occurrences.drop(['Occurrence', 'index'], axis=1)
 
-    def __get_number_of_clear_decision(self, features_occurrence: pd.DataFrame,
-                                       features_decisions_occurrence: pd.DataFrame) -> pd.DataFrame:
+    def __get_number_of_clear_decisions(self, features_occurrence: pd.DataFrame,
+                                        features_decisions_occurrence: pd.DataFrame) -> pd.DataFrame:
         """
         Calculates number of sets of attributes that did not lead to conflicting decisions for each class\n
         :param features_occurrence: dataframe with sets of features and their occurrence count in a dataset
@@ -68,87 +68,89 @@ class InconsistenciesRemover(object):
         target_label = 'Decision'
         features_certain_decision = self.__get_certain_decision_rows(features_occurrence, features_decisions_occurrence)
 
-        decisions = features_certain_decision[target_label]
-        features_certain_decision = features_certain_decision.drop(columns=target_label)
-        features_decisions_occurrence = features_decisions_occurrence.drop(columns=target_label)
-
         merged_decisions = pd.merge(
             features_decisions_occurrence,
             features_certain_decision,
             on=self.feature_labels,
-            suffixes=[])
-        merged_decisions[target_label] = decisions
+            )
 
-        number_of_clear_decision = pd.DataFrame(
+        merged_decisions = merged_decisions.drop(['Decision_y'], axis=1).rename(
+            index=str,
+            columns={
+                "Decision_x": "Decision",
+                "Occurrence_x": "Occurrence"
+            })
+
+        number_of_clear_decisions = pd.DataFrame(
             merged_decisions.groupby([target_label], as_index=False)['Occurrence'].agg(np.sum))
 
-        return number_of_clear_decision
+        return number_of_clear_decisions
 
-    def solve_conflicts(self, number_of_conflicts_decision, problems_to_solve,
-                        features_decisions_occurence, number_of_clear_decision, general_features_occurence):
-
-        for _, row in number_of_conflicts_decision.iterrows():
-            new_df = pd.DataFrame(columns={"Decision", "Probability"})
+    def __solve_conflicts(self, nums_of_conflicting_decisions: pd.DataFrame,
+                          problems_to_solve: pd.DataFrame,
+                          features_decisions_occurrence: pd.DataFrame,
+                          num_of_clear_decisions: pd.DataFrame) -> pd.DataFrame:
+        """
+        Changes decisions for conflicting rows to those occurring more often\n
+        :param nums_of_conflicting_decisions: sets of features that cause conflicts and their occurrence count
+        :param problems_to_solve: rows with conflicts to resolve
+        :param features_decisions_occurrence: dataframe with features of objects, decisions for them and count of those instances
+        :param num_of_clear_decisions: counts of decisions for all classes that did not cause any conflicts
+        :return: dataframe with resolved conflicts
+        """
+        proba_columns = ['Decision', 'Probability']
+        for _, row in nums_of_conflicting_decisions.iterrows():
+            proba_df = pd.DataFrame(columns=proba_columns)
 
             for _, row_2 in problems_to_solve.iterrows():
-                if (row[self.feature_labels].values == row_2[self.feature_labels]).all():
+                is_same_row = (row[self.feature_labels].values == row_2[self.feature_labels]).all()
+                if is_same_row:
+                    if row_2['Decision'] in num_of_clear_decisions['Decision'].values:
+                        occurrence = num_of_clear_decisions.loc[
+                            num_of_clear_decisions['Decision'] == row_2['Decision']
+                            ]['Occurrence'].values.item()
+                    else:
+                        occurrence = 0
 
-                    try:
-                        occurence = (number_of_clear_decision.loc[
-                            number_of_clear_decision['Decision'] == row_2[
-                                ['Decision']].values[0]]).values[0][1]
-                    except:
-                        occurence = 0
-
-                    probability = occurence / len(self.decision_table)
-                    new_df = new_df.append({
-                        'Decision': row_2[['Decision']].values,
+                    probability = occurrence / len(self.decision_table)
+                    proba_df = proba_df.append({
+                        'Decision': row_2['Decision'],
                         'Probability': probability
-                    },
-                        ignore_index=True)
+                    }, ignore_index=True)
 
-            new_value = new_df.loc[new_df['Probability'].idxmax()]['Decision'][0]
-            for idx, row_decision_table in features_decisions_occurence.iterrows():
+            new_value = proba_df.loc[
+                proba_df['Probability'].idxmax()
+            ]['Decision'].item()
+
+            for idx, row_decision_table in features_decisions_occurrence.iterrows():
                 if (row[self.feature_labels].values == row_decision_table[self.feature_labels]).all():
-                    if features_decisions_occurence.loc[features_decisions_occurence.index == idx].Decision.values[
-                        0] != new_value:
-                        # if self.settings.show_results:
-                        # print("Current value: {}".format(features_decisions_occurence.loc[features_decisions_occurence.index == idx].Decision.values[0]))
-                        # print("New value: {}".format(new_value))
-                        # for idy, row_general_occurence in general_features_occurence.iterrows():
-                        #     if (row_general_occurence[self.feature_labels].values == row_decision_table[self.feature_labels]).all():
-                        #         if self.settings.show_results:
-                        #             print(row_general_occurence.Occurence)
-                        #         break
-                        features_decisions_occurence.loc[idx, 'Decision'] = new_value
+                    if row_decision_table['Decision'] != new_value:
+                        features_decisions_occurrence.loc[idx, 'Decision'] = new_value
                         self.changed_decisions = self.changed_decisions + 1
 
-        return features_decisions_occurence
+        return features_decisions_occurrence
 
     def inconsistencies_removing(self):
         features_decisions_occurrence = self.__get_occurrence_of_rows(self.decision_table, None)
         features_decisions_occurrence = features_decisions_occurrence.drop(['index'], axis=1)
 
-        general_features_occurence = features_decisions_occurrence.copy()
         self.samples = sum(features_decisions_occurrence['Occurrence'])
         feature_sets_occurrence = self.__get_occurrence_of_rows(self.decision_table, ['Decision'])
 
         nums_of_conflicting_decisions = feature_sets_occurrence[feature_sets_occurrence['Occurrence'] > 1]
-
-        num_of_clear_decision = self.__get_number_of_clear_decision(feature_sets_occurrence,
-                                                                    features_decisions_occurrence)
-
+        num_of_clear_decisions = self.__get_number_of_clear_decisions(feature_sets_occurrence,
+                                                                      features_decisions_occurrence)
         problems_to_solve = pd.merge(
             features_decisions_occurrence,
             nums_of_conflicting_decisions,
             how='inner',
             on=self.feature_labels).drop(['Occurrence_x', "Occurrence_y"], axis=1)
 
-        features_decisions_occurence = self.solve_conflicts(
+        features_decisions_occurrence = self.__solve_conflicts(
             nums_of_conflicting_decisions, problems_to_solve,
-            features_decisions_occurrence, num_of_clear_decision, general_features_occurence)
-        decision_table = features_decisions_occurence.drop(['Occurrence'],
-                                                           axis=1).drop_duplicates(
+            features_decisions_occurrence, num_of_clear_decisions)
+        decision_table = features_decisions_occurrence.drop(['Occurrence'],
+                                                            axis=1).drop_duplicates(
             keep='first',
             inplace=False)
 
