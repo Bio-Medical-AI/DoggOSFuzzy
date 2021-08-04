734bfdc72301735d7ebda5c5c10c2208df190367
Adam Chlopowiec <yeabunnypl@gmail.com>
2021-08-04 12:37:38 +0200

    Changed induction module structure

--- doggos/induction/fuzzy_decision_table_generator.py
+++ doggos/induction/fuzzy_decision_table_generator.py
@@ -7,7 +7,7 @@ from doggos.knowledge import Clause, LinguisticVariable, Domain
 
 class FuzzyDecisionTableGenerator:
     """
-    Class used to fuzzify dataset of crisp values into fuzzy decision table for rule induction system
+    Class used to fuzzify dataset of crisp values into fuzzy decision table for rule induction system:
     https://www.mdpi.com/2076-3417/11/8/3484 - 2.2.3. Rule Induction with Information Systems
 
     Attributes
@@ -34,25 +34,28 @@ class FuzzyDecisionTableGenerator:
     __features: List[LinguisticVariable]
     __features_clauses: Dict[str, List[Clause]]
 
-    def __init__(self, X: pd.DataFrame, y: pd.Series):
+    def __init__(self, X: pd.DataFrame, y: pd.Series, domain: Domain):
         """
-        Create FuzzyDecisionTableGenerator with given fuzzy sets, data and target values\n
+        Create FuzzyDecisionTableGenerator with given fuzzy sets, data and target values.
+
         :param X: data representing objects
         :param y: target values for corresponding objects
+        :param domain: domain of dataset features
         """
         self.__fuzzy_sets = None
         self.__dataset = X
         for column in self.__dataset.columns:
             self.__dataset.rename({column: str(column)})
 
-        self.__features = [LinguisticVariable(feature_name, Domain(0, 1.001, 0.001))
+        self.__features = [LinguisticVariable(feature_name, domain)
                            for feature_name in self.__dataset.columns]
         self.__features_clauses = {feature_name: [] for feature_name in self.__dataset.columns}
         self.__dataset['Decision'] = y
 
     def __get_highest_membership(self, feature: str, crisp: float) -> str:
         """
-        Determines a name of a fuzzy set with the highest membership value for a given feature\n
+        Determines a name of a fuzzy set with the highest membership value for a given feature.
+
         :param feature: a name of a feature for which to determine a name of the highest membership set
         :param crisp: a crisp value of the feature
         :return: a name of a fuzzy set with the highest membership value for a given feature
@@ -68,7 +71,8 @@ class FuzzyDecisionTableGenerator:
 
     def __fuzzify_dataset(self) -> pd.DataFrame:
         """
-        Replaces crisp values of a dataset with names of highest membership fuzzy sets\n
+        Replaces crisp values of a dataset with names of highest membership fuzzy sets.
+
         :return: dataset with crisp values replaced by names of highest membership fuzzy sets
         """
         fuzzy_dataset = pd.DataFrame(columns=self.__dataset.columns, dtype='string')
@@ -85,7 +89,8 @@ class FuzzyDecisionTableGenerator:
 
     def fuzzify(self, fuzzy_sets: Dict[str, Dict]) -> pd.DataFrame:
         """
-        Performs fuzzification on given dataset and returns fuzzified decision table\n
+        Performs fuzzification on given dataset and returns fuzzified decision table.
+
         :param fuzzy_sets: fuzzy sets that are describing columns of given dataset
         :return: dataset with crisp values replaced by names of highest membership fuzzy sets
         """
--- doggos/induction/information_system.py
+++ doggos/induction/information_system.py
@@ -1,2 +1,27 @@
+from typing import List
+
+import pandas as pd
+
+from doggos.induction import FuzzyDecisionTableGenerator
+from doggos.induction.inconsistencies_remover import InconsistenciesRemover
+from doggos.induction.rule_builder import RuleBuilder
+from doggos.knowledge import Domain
+
+
 class InformationSystem:
-    pass
+    def __init__(self, X: pd.DataFrame, y: pd.Series, feature_labels: List[str]):
+        self.X = X
+        self.y = y
+        self.feature_labels = feature_labels
+        self.decision_table_generator = None
+        self.inconsistencies_remover = None
+        self.rule_builder = None
+
+    def induce_rules(self, fuzzy_sets, domain: Domain):
+        self.decision_table_generator = FuzzyDecisionTableGenerator(self.X, self.y)
+        decision_table = self.decision_table_generator.fuzzify(fuzzy_sets)
+        self.inconsistencies_remover = InconsistenciesRemover(decision_table, self.feature_labels)
+        consistent_decision_table = self.inconsistencies_remover.remove_inconsistencies()
+        self.rule_builder = RuleBuilder(consistent_decision_table)
+        rules, antecedents = self.rule_builder.induce_rules(fuzzy_sets)
+        return rules, antecedents
